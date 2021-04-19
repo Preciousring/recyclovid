8cea6514d882f8b37026e3a5186fb02760af94ae
Preciousring <preciousring44@gmail.com>
2021-04-19 18:58:44 +0200

    Give Mask Form

--- src/Controller/GiveMaskController.php
+++ src/Controller/GiveMaskController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Deposit;
+use App\Entity\DepositContent;
+use App\Entity\MaskType;
 use App\Form\GiveMaskFormType;
-
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,14 +15,45 @@ class GiveMaskController extends AbstractRecycloController
 {
     /**
      * @Route("/give-mask", name="give_mask")
+     * @param Request $request
+     * @return Response
      */
-    public function index(): Response
+    public function testAction(Request $request): Response
     {
         $deposit = new Deposit();
         $form = $this->createForm(GiveMaskFormType::class, $deposit);
+        $form->handleRequest($request);
 
-        return $this->render('give_mask/index.html.twig',$this->twigParams +[
+        if ($form->isSubmitted() && $form->isValid()) {
+            $deposit->setUser($this->security->getUser());
+            $this->em->persist($deposit);
+
+            $hasMaskTypeGiven = false;
+            $maskTypes = $this->em->getRepository(MaskType::class)->findAll();
+            /**
+             * @var MaskType $maskType
+             */
+            foreach ($maskTypes as $maskType) {
+                if (intval($qty = $form->get('depositContent' . $maskType->getId())->getData())) {
+                    $depositContent = new DepositContent();
+                    $depositContent->setMaskType($maskType);
+                    $depositContent->setDeposit($deposit);
+                    $depositContent->setQuantity($qty);
+                    $this->em->persist($depositContent);
+                    $hasMaskTypeGiven = true;
+                }
+            };
+
+            if ($hasMaskTypeGiven) {
+                $this->em->flush();
+                return $this->redirectToRoute('thank-you');
+            } else {
+                $form->addError(new FormError('Vous devez indiquer au moins une quantité de masques.'));
+            }
+        }
+
+        return $this->render('give_mask/index.html.twig', $this->twigParams + [
                 'giveMaskForm' => $form->createView(),
-        ]);
+            ]);
     }
 }
--- src/Form/GiveMaskFormType.php
+++ src/Form/GiveMaskFormType.php
@@ -3,18 +3,46 @@
 namespace App\Form;
 
 use App\Entity\Deposit;
+use App\Entity\MaskType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class GiveMaskFormType extends AbstractType
 {
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('date')
-            ->add('depositLocation')
-        ;
+            ->add('date', DateTimeType::class, ['label' => 'Date et heure de Dépôt'])
+            ->add('depositLocation', null, [
+                'label' => 'Lieu de Dépôt',
+                'placeholder' => 'Choisissez une option',
+            ]);
+
+        /**
+         * @var MaskType $maskType
+         */
+        foreach ($this->em->getRepository(MaskType::class)->findAll() as $maskType) {
+            $builder->add('depositContent' . $maskType->getId(), IntegerType::class, [
+                'label' => 'Nb de masque de type ' . $maskType->getLabel(),
+                'mapped' => false,
+                'required' => false
+            ]);
+        }
+
+        $builder->add('submit', SubmitType::class, ['label' => 'Je contribue !']);
     }
 
     public function configureOptions(OptionsResolver $resolver)
